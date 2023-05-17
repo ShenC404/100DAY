37a7c84afb14fd6a2d058bc01e0ed1e40c392549
ShenC <113698036+ShenC404@users.noreply.github.com>
2023-05-17 08:51:42 +0800

    Add files via upload

---
+++ day01.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main() 
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d",&a,&b);
+//	int hello = 0;
+//	hello = a + b;
+//	printf("%d",hello);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("D:\\100days");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;						//这里的"a"是变量
+//	printf("今天是晴天吗？\n");
+//	printf("（是晴天扣1）\n");
+//	scanf("%d",&a);
+//	if (a == 1)
+//		printf("太棒了！希望你的心情和天气一样晴朗！\n");
+//	else
+//		printf("没关系！今天要有个好心情！\n");
+//	return 0;
+//}
