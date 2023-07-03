d5d32a795faf80d38378b331d52082ac8db849fa
ShenC <113698036+ShenC404@users.noreply.github.com>
2023-07-03 09:38:57 +0800

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int Add(int a,int b)
+//{
+//	int c = 0;
+//	c = a + b;
+//	return c;
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int d = Add(a,b);
+//	printf("%d", d);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char a = '0';
+//	printf("今天是晴天吗？\n");
+//	scanf("%c",&a);
+//	if (a == "是")
+//		printf("太棒了！希望你的心情和天气一样晴朗！\n");
+//	else
+//		printf("没关系！今天要有个好心情！\n");
+//	return 0;
+//}
